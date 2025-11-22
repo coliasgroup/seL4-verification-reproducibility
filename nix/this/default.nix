0faf131be5d04725605bbfb06e64b9d246fd40e8
Nick Spinale <nick@nickspinale.com>
2025-11-22 00:09:12 -0800

    x

--- nix/this/default.nix
+++ nix/this/default.nix
@@ -61,7 +61,7 @@ rec {
           "create_frames_of_region"
         ];
         "ARM-O1-arm-none-eabi-gcc-14.2.0" = [ "init_freemem" ];
-        "ARM-O2-arm-none-eabi-gcc-14.2.0" = [ "init_freemem" "decodeARMMMUInvocation" ]; # untested
+        "ARM-O2-arm-none-eabi-gcc-14.2.0" = [ ]; # untested
       }."${bvName}-${targetCC.name}" or (lib.warn "bvExclude not specified for ${bvName}" null))
     }:
     {
