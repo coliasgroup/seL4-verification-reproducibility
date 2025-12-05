518329cc50cf7ac3ae5ee9673c3e4dba49cc0bb0
Nick Spinale <nick@nickspinale.com>
2025-12-05 02:17:53 -0800

    unfocus

--- nix/this/scope/decompilation.nix
+++ nix/this/scope/decompilation.nix
@@ -39,11 +39,11 @@ let
 
   keep = "invokeCNodeCancelBadgedSends";
 
-  ignoreFile = runCommand "ignore" {} ''
-    cat ${kernel}/kernel.sigs | cut -d ' ' -f 2 | grep -v ${keep} | tr '\n' ',' | sed 's/,$/\n/' > $out
-  '';
+  # ignoreFile = runCommand "ignore" {} ''
+  #   cat ${kernel}/kernel.sigs | cut -d ' ' -f 2 | grep -v ${keep} | tr '\n' ',' | sed 's/,$/\n/' > $out
+  # '';
 
-  # ignoreFile = writeText "ignore" (lib.concatStringsSep "," ignoreList);
+  ignoreFile = writeText "ignore" (lib.concatStringsSep "," ignoreList);
 
   scriptIn = writeText "x.sml" ''
     load "decompileLib";
