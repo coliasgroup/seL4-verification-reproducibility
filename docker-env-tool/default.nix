3aaf20a9e2916a686fb7abd2532e3899131c2d3d
Nick Spinale <nick@nickspinale.com>
2026-06-01 14:56:15 -0700

    Remove docker-env-tool

--- docker-env-tool/default.nix
+++
@@ -1,9 +0,0 @@
-let
-  pkgs = import ../nixpkgs {};
-
-  runInContainer = pkgs.callPackage ./run-in-container.nix {};
-
-  probe = pkgs.callPackage ./probe.nix {};
-
-in
-runInContainer // probe
--- docker-env-tool/run-in-container.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu -o pipefail
-
-here=$(dirname $0)
-
-run=$(nix-build $here -A run --no-out-link)
-
-exec $run/bin/run-in-container "$@"
