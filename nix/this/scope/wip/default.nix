ee45a58890bc43a39eef9ee2bd184a331faebfcb
Nick Spinale <nick@nickspinale.com>
2026-06-02 16:31:10 -0700

    x

--- nix/this/scope/wip/default.nix
+++ nix/this/scope/wip/default.nix
@@ -83,7 +83,7 @@ in rec {
   )));
 
   stackBoundsScopes = lib.flip lib.filter decompScopes (
-    # TODO graph-refine can't figure out mutual recursion for clang or gcc 15+ codegen
+    # TODO graph-refine can't figure out mutual recursion for clang or gcc 15+ -O2 codegen
     scope.scopeConfig.arch == "ARM" && (
       scope.scopeConfig.targetCCIsGCC && (lib.versionOlder targetCC.version "15" || scope.scopeConfig.optLevel == "-O1")
     )
