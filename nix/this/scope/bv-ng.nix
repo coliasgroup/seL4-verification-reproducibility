2c9e3fa037830dae020847ab532d6df22725f141
Nick Spinale <nick@nickspinale.com>
2026-06-08 01:58:01 -0700

    Export whole bv-ng expr

--- nix/this/scope/bv-ng.nix
+++ nix/this/scope/bv-ng.nix
@@ -1,11 +1,3 @@
 {}:
 
-let
-  topLevel = import ../../../projects/binary-verification/nix;
-in {
-  inherit (topLevel)
-    sel4-bv
-    sel4-bv-test
-    distrib
-  ;
-}
+import ../../../projects/binary-verification/nix
